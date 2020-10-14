b3ee0d39bfde7832443c913304f6fea025bc9b4f
Prashant Solanki <prashant.solanki@amc.local>
2020-10-14 23:40:20 +1100

    Multiple Changes: Converted to ES6, Updated dependencies, added babel, eslint, prettier, etc.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
-module.exports = {
-    "extends": "airbnb-base"
+export default {
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+  parser: 'eslint-parser',
+  // parserOptions: {
+  //   sourceType: 'module',
+  //   allowImportExportEverywhere: false,
+  //   ecmaFeatures: {
+  //     globalReturn: false,
+  //   },
+  //   babelOptions: {
+  //     configFile: 'path/to/config.js',
+  //   },
+  // },
 };
